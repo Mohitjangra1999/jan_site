eb340e9c5d001e5f13b14565d7dbde94e73683d0
mohit <99mohitjangra@gmail.com>
2019-01-30 21:53:49 +0530

    first commit

---
+++ books/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import  HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>This is homepage lala </h1>")
+
+def new_fun(request):
+    return HttpResponse("<h1>This is new homepage</h1>")
