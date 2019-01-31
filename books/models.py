eecc05ec7e261b38941c80fd07c2c14ec4de3ea6
mohit <99mohitjangra@gmail.com>
2019-01-31 22:52:34 +0530

    ne commit

--- books/models.py
+++ books/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Book(models.Model):
+
+    def __str__(self):
+        return self.name + "-" +self.price
+
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
