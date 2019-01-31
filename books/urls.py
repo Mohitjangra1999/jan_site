9914602d25d501eba32f53a845598cfa0a63edb8
mohit <99mohitjangra@gmail.com>
2019-02-01 00:33:47 +0530

    completed

--- books/urls.py
+++ books/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+        url(r'^(?P<book_id>[0-9]+)/$',views.details,name='details'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Book
 from django.http import  HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("<h1>Hello Mr. jangra, Kaisa hai laadle</h1>")
+    all_books = Book.objects.all()
+    temp = loader.get_template("books/index.html")
+    cont = {
+        "all_books": all_books
+    }
+    return HttpResponse(temp.render(cont, request))
+
+def details(request,book_id):
+    return HttpResponse("<h1>You requested info for book " + str(book_id) +"</h1>")
+
