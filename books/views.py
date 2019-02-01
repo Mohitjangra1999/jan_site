208de9bf1e4bdba6e2460edd3b5f587d9cac35de
mohit <99mohitjangra@gmail.com>
2019-02-01 12:59:06 +0530

    cc

--- books/views.py
+++ books/views.py
@@ -3,7 +3,6 @@ from .models import Book
 from django.http import  HttpResponse
 from django.template import loader
 
-'''
 def index(request):
     all_books = Book.objects.all()
     temp = loader.get_template("books/index.html")
@@ -11,9 +10,7 @@ def index(request):
         "all_books": all_books
     }
     return HttpResponse(temp.render(context, request))
-    '''
-def index(request):
-    return HttpResponse("<h1>I am awesome</h1>")
+
 
 def details(request,book_id):
     return HttpResponse("<h1>You requested info for book " + str(book_id) +"</h1>")
