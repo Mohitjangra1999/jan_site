074be1018a96c83b6d590bcf4e64a7d30639adbe
mohit <99mohitjangra@gmail.com>
2019-02-01 18:16:50 +0530

    iii

--- books/views.py
+++ books/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render
+
 from .models import Book
-from django.http import  HttpResponse
+from django.http import Http404
 from django.shortcuts import render
-from django.http  import Http404
 
 def index(request):
     all_books = Book.objects.all()
 
     context = {
-        "all_books": all_books
+        "all_books": all_books,
     }
-    return render(request,"books/index.html",context)
+    return render(request, "books/index.html", context)
+
 
 def details(request, book_id):
     try:
         x = Book.objects.get(id=book_id)
     except Book.DoesNotExist:
         raise Http404("This book does not exists.")
-    return render(request, "books/details.html", {"book":x})
+    return render(request, "books/details.html", {"book": x})
 
