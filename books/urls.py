8583d105bd7caa885b586faf6ee424bf17e3e850
mohit <99mohitjangra@gmail.com>
2019-02-02 22:24:19 +0530

    many changes

--- books/urls.py
+++ books/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
-app_name='books'
+app_name = 'books'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<book_id>[0-9]+)/$', views.details, name='detail'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,21 +1,29 @@
 
 from .models import Book
-from django.http import Http404
-from django.shortcuts import render
-
+from django.views import generic
+'''
 def index(request):
     all_books = Book.objects.all()
 
     context = {
         "all_books": all_books,
     }
-    return render(request, "books/index.html", context)
+    return render(request, "books/index.html", context) '''
 
+class IndexView(generic.ListView):
+    template_name = 'books/index.html'
 
+    def get_queryset(self):
+        return Book.objects.all()
+'''
 def details(request, book_id):
     try:
         x = Book.objects.get(id=book_id)
     except Book.DoesNotExist:
         raise Http404("This book does not exists.")
-    return render(request, "books/details.html", {"book": x})
+    return render(request, "books/details.html", {"book": x}) '''
+
+class DetailView(generic.DetailView):
+    template_name = 'books/details.html'
+    model = Book
 
