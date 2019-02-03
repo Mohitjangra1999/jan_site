c976fc3c93199475195341ff75f67029fa634244
mohit <99mohitjangra@gmail.com>
2019-02-04 00:39:54 +0530

    making add book button functional

--- books/models.py
+++ books/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Book(models.Model):
+
+    def get_absolute_url(self):
+        return reverse('books:detail', kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.name + "-" + self.price
@@ -10,4 +14,4 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
-    image= models.CharField(max_length=100)
+    image = models.CharField(max_length=1000000000000000000)
--- books/views.py
+++ books/views.py
@@ -1,27 +1,17 @@
 
 from .models import Book
 from django.views import generic
-'''
-def index(request):
-    all_books = Book.objects.all()
-
-    context = {
-        "all_books": all_books,
-    }
-    return render(request, "books/index.html", context) '''
+from django.views.generic.edit import CreateView
 
 class IndexView(generic.ListView):
     template_name = 'books/index.html'
 
     def get_queryset(self):
         return Book.objects.all()
-'''
-def details(request, book_id):
-    try:
-        x = Book.objects.get(id=book_id)
-    except Book.DoesNotExist:
-        raise Http404("This book does not exists.")
-    return render(request, "books/details.html", {"book": x}) '''
+
+class BookCreate(CreateView):
+    model = Book
+    fields = ['name', 'author', 'price', 'type', 'image']
 
 class DetailView(generic.DetailView):
     template_name = 'books/details.html'
